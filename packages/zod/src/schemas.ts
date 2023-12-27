8da3df551e6646b825c685bc0ad5544d7e4c3c44
m10rten <qmaartenenzo@gmail.com>
2023-12-27 23:58:49 +0100

    Add Account and Session schemas to User schema

--- packages/zod/src/schemas.ts
+++ packages/zod/src/schemas.ts
@@ -7,15 +7,40 @@ export const Base = z.object({
 });
 export type Base = z.infer<typeof Base>;
 
+export const Account = Base.extend({
+  userId: z.string().uuid(),
+  type: z.string(),
+  provider: z.string(),
+  providerAccountId: z.string(),
+  refresh_token: z.string().optional().nullable(),
+  access_token: z.string().optional().nullable(),
+  expires_at: z.number().optional().nullable(),
+  token_type: z.string().optional().nullable(),
+  scope: z.string().optional().nullable(),
+  id_token: z.string().optional().nullable(),
+  session_state: z.string().optional().nullable(),
+});
+export type Account = z.infer<typeof Account>;
+
+export const Session = Base.extend({
+  sessionToken: z.string().uuid(),
+  userId: z.string().uuid(),
+  expires: z.date(),
+});
+export type Session = z.infer<typeof Session>;
+
 export const User = Base.extend({
-  id: z.string().uuid(),
   name: z
     .string({ invalid_type_error: "Invalid name" })
     .min(3, { message: "Name must be at least 3 characters long" })
     .max(255, { message: "Name must be at most 255 characters long" }),
-  email: z.string().email({ message: "Invalid email" }),
-  ip: z.string().ip({ message: "Invalid IP" }).optional().nullable(),
-  createdAt: z.date().default(() => new Date()),
-  updatedAt: z.date().default(() => new Date()),
+  email: z
+    .string()
+    .email({ message: "Invalid email" })
+    .max(255, { message: "Email cannot be longer than 255 characters" }),
+  emailVerified: z.date().optional().nullable(),
+  image: z.string().optional().nullable(),
+  accounts: z.array(Account).optional().nullable(),
+  sessions: z.array(Session).optional().nullable(),
 });
 export type User = z.infer<typeof User>;
