9b0480e0d2404bffd855b452de195c403df414c0
m10rten <qmaartenenzo@gmail.com>
2024-01-07 23:26:58 +0100

    Update styles and text sizes in showcase app

--- apps/showcase/src/app/page.tsx
+++ apps/showcase/src/app/page.tsx
@@ -5,11 +5,13 @@ import { Button } from "#/ui/button";
 export default function Home() {
   return (
     <main className="w-full h-full container flex flex-col justify-center items-center gap-4">
-      <span className="text-2xl font-medium text-indigo-600 dark:text-indigo-400">
+      <span className="text-xl sm:text-2xl font-medium text-indigo-600 dark:text-indigo-400">
         Introducing
       </span>
-      <h1 className="text-5xl font-bold text-center">A New Intuitive Way Of Building</h1>
-      <p className="text-xl max-w-xl text-center text-foreground/80">
+      <h1 className="text-4xl sm:text-5xl font-bold text-center">
+        A New Intuitive Way Of Building
+      </h1>
+      <p className="text-lg sm:text-xl max-w-xl text-center text-foreground/80">
         A new way of building websites, apps, and products with a simple, intuitive, and
         powerful API. With this suite of packages, you can build anything you want,
         Intuitively.
