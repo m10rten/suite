826a94146b2d7977811f3e79dd91b1db8878a1c0
m10rten <qmaartenenzo@gmail.com>
2024-01-13 16:35:04 +0100

    Moved log from internal to public packages

---
+++ .changeset/happy-spies-sip.md
@@ -0,0 +1,5 @@
+---
+"@mvdlei/log": patch
+---
+
+Recreate package
--- packages/log/index.ts
+++ packages/log/index.ts
@@ -22,6 +22,29 @@ export const defaultOptions = {
   level: "debug",
 } satisfies LoggerOptions;
 
+let _logger: winston.Logger | undefined = undefined;
+
+/**
+ * Will always return a new logger instance, use `assert` to get the same instance every time
+ * @param options
+ * @returns
+ */
 export const create = (options: LoggerOptions = defaultOptions) => {
   return createLogger({ ...options, ...defaultOptions });
 };
+
+/**
+ * Will save the logger instance and return it every time or create a new one if it doesn't exist
+ * @param options
+ * @returns
+ */
+export const assert = (options: LoggerOptions) => {
+  if (_logger) return _logger;
+  _logger = create(options);
+  return _logger;
+};
+
+/**
+ * Logger instance to easily log messages
+ */
+export const logger = assert(defaultOptions);
