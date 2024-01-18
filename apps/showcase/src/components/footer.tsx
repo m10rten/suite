4db4b8f3c267c55145ebd82d70a19ed80bd120d2
m10rten <qmaartenenzo@gmail.com>
2024-01-18 14:24:06 +0100

    Add attribution to footer

--- apps/showcase/src/components/footer.tsx
+++ apps/showcase/src/components/footer.tsx
@@ -7,10 +7,11 @@ import { cn } from "@/lib/utils";
 export function Footer() {
   return (
     <footer className="flex relative items-center justify-center py-4 footer border-t bg-background/60">
+      <p>Made with passion by</p>
       <Link
         href={"https://github.com/m10rten"}
         target="_blank"
-        className="hover:underline underline-offset-2 flex justify-center gap-1 items-center">
+        className="ml-2 sm:ml-3 hover:underline underline-offset-2 flex justify-center gap-1 items-center">
         <span className={cn(GeistMono.className)}>© m10rten</span>
         <ExternalLink className="inline-block ml-2" size={16} />
       </Link>
