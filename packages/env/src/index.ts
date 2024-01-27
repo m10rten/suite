418e6d5d62cc183ee3778b945917bfb5b7fc458c
Maarten <57992191+m10rten@users.noreply.github.com>
2024-01-27 20:55:45 +0100

    Update index.ts

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -5,6 +5,11 @@
  * @example
  * import { define } from "@mvdlei/env";
  * export const env = define({}); // Define your env vars here.
+ *
+ * Or just use Env out of the box
+ * @example
+ * const env = Env.init();
+ * const myVar = env.get("KEY");
  */
 
 export * from "./env";
