8a570f18b02fe075e939924acfb689e08384bb61
m10rten <qmaartenenzo@gmail.com>
2024-02-03 13:12:32 +0100

    Add Edge package init

---
+++ packages/edge/src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Everything you need to build an Edge-ready application.
+ * @packageDocumentation
+ * @module @mvdlei/edge
+ * @preferred
+ * @example
+ * ```ts
+ * import { Edge } from '@mvdlei/edge';
+ * const edge = new Edge();
+ *
+ * export const { POST } = edge.api.handle((req, res) => {
+ *    return res.json({ message: 'Hello, World!' });
+ * })
+ *
+ * ```
+ */
+export const Edge = {};
