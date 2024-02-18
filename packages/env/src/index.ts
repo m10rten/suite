5d07627594747d55ec31595929224f43a1fad926
m10rten <qmaartenenzo@gmail.com>
2024-02-18 22:16:16 +0100

    update jsdoc

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -3,13 +3,12 @@
  * @file Index.ts
  * @description The entry point for the `@mvdlei/env` package.
  * @example
- * import { define } from "@mvdlei/env";
+ * import { define, Env } from "@mvdlei/env";
  * export const env = define({}); // Define your env vars here.
  *
  * Or just use Env out of the box
  * @example
- * const env = Env.init();
- * const myVar = env.get("KEY");
+ * const myVar = Env.get("KEY");
  */
 
 export * from "./env";
