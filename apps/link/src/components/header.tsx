2b51cc9284db2109166651a61b58ec4bf996af38
m10rten <qmaartenenzo@gmail.com>
2024-02-23 18:25:09 +0100

    update styling nav logo

--- apps/link/src/components/header.tsx
+++ apps/link/src/components/header.tsx
@@ -12,12 +12,12 @@ export default async function Header() {
     <header className="fixed top-0 w-full border-b py-2 px-4 z-50 backdrop-blur-md bg-background/80 shadow-sm">
       <nav className="w-full justify-between flex items-center my-1 sm:container">
         <span className="flex font-mono text-lg">
-          <span className="hidden sm:block">Mvdlei / </span>
+          <span className="hidden sm:block">Mvdlei /&nbsp;</span>
           <Link href={"/"}>
-            <h1 className="font-bold">&nbsp;Link</h1>
+            <h1 className="font-bold">Link</h1>
           </Link>
         </span>
-        <div className="flex items-center sm:gap-5 gap-3 group sm:px-3 px-1 text-sm sm:text-base">
+        <div className="flex items-center sm:gap-4 gap-2 group sm:px-3 px-1 text-sm sm:text-base">
           {session ? (
             <Profile session={session} />
           ) : (
