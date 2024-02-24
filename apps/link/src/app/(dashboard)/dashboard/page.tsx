9ec2d12abf6d6fae13b71c7c2ba12edee8031bc4
m10rten <qmaartenenzo@gmail.com>
2024-02-24 17:04:56 +0100

    init dashboard page

---
+++ apps/link/src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default async function DashboardPage() {
+  return (
+    <>
+      <main className="flex flex-1 w-full flex-col items-center justify-center mt-24 mb-8">
+        <h1 className="text-4xl font-bold">Dashboard</h1>
+      </main>
+    </>
+  );
+}
