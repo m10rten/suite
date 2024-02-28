a41703c845f08d364f595a73d003b4d55137219b
m10rten <qmaartenenzo@gmail.com>
2024-02-28 12:36:35 +0100

    add basic not-found page

---
+++ apps/link/src/app/[slug]/not-found.tsx
@@ -0,0 +1,8 @@
+export default async function NotFoundSlug() {
+  return (
+    <main className="flex flex-1 w-full flex-col items-center justify-center mt-24 mb-8">
+      <h1 className="text-4xl font-extrabold text-gray-900">404 - Not Found</h1>
+      <p className="text-2xl font-bold text-gray-700">This link does not exist</p>
+    </main>
+  );
+}
