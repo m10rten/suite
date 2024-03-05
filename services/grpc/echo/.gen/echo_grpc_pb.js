69c675b5a8b77f3a4a889d97ef12012e5289c2c4
m10rten <qmaartenenzo@gmail.com>
2024-03-05 18:54:36 +0100

    eslint fixes

--- services/grpc/echo/.gen/echo_grpc_pb.js
+++ services/grpc/echo/.gen/echo_grpc_pb.js
@@ -1,12 +1,12 @@
 // GENERATED CODE -- DO NOT EDIT!
 
-'use strict';
-var grpc = require('@grpc/grpc-js');
-var echo_pb = require('./echo_pb.js');
+"use strict";
+var grpc = require("@grpc/grpc-js");
+var echo_pb = require("./echo_pb.js");
 
 function serialize_echo_Message(arg) {
   if (!(arg instanceof echo_pb.Message)) {
-    throw new Error('Expected argument of type echo.Message');
+    throw new Error("Expected argument of type echo.Message");
   }
   return Buffer.from(arg.serializeBinary());
 }
@@ -15,10 +15,9 @@ function deserialize_echo_Message(buffer_arg) {
   return echo_pb.Message.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-
-var EchoServiceService = exports.EchoServiceService = {
+var EchoServiceService = (exports.EchoServiceService = {
   echo: {
-    path: '/echo.EchoService/Echo',
+    path: "/echo.EchoService/Echo",
     requestStream: false,
     responseStream: false,
     requestType: echo_pb.Message,
@@ -28,6 +27,6 @@ var EchoServiceService = exports.EchoServiceService = {
     responseSerialize: serialize_echo_Message,
     responseDeserialize: deserialize_echo_Message,
   },
-};
+});
 
 exports.EchoServiceClient = grpc.makeGenericClientConstructor(EchoServiceService);
