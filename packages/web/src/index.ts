3954ed4ac8b40a7d2160f1b367567175490b21ff
m10rten <qmaartenenzo@gmail.com>
2024-03-17 13:13:33 +0100

    Refactor code and remove test code

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 export * from "./fetch";
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
@@ -45,3 +46,42 @@ export namespace Web {
     }
   }
 }
+
+/**
+ * Test code:
+ */
+// const main = async () => {
+//   try {
+//     console.log(Web.Api.Origin.fromEnv());
+
+//     console.log("started");
+
+//     const todo = await api("1//?q=1", {
+//       origin: "https://jsonplaceholder.typicode.com//1",
+//       path: "//todos//",
+//       params: {
+//         h: "2",
+//       },
+//     });
+//     console.log(todo.status, todo.data);
+
+//     const get = await api.get("https://jsonplaceholder.typicode.com/todos/2");
+//     console.log(get.status, get.data);
+
+//     const post = await api.post("https://jsonplaceholder.typicode.com/todos", {
+//       body: JSON.stringify({
+//         title: "foo",
+//         body: "bar",
+//         userId: 1,
+//       }),
+//     });
+//     console.log(post.status, post.data);
+//   } catch (error) {
+//     if (HttpError.is(error)) {
+//       console.log(error.response.statusText);
+//     } else {
+//       console.error(error);
+//     }
+//   }
+// };
+// main().then(() => console.log("done"));
