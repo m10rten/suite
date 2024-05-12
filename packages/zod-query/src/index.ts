ede373b562a788a6a3fc403eb8b4cab6302de9d5
m10rten <qmaartenenzo@gmail.com>
2024-05-12 12:13:38 +0200

    chore: Update zod-query package version to 0.0.5

--- packages/zod-query/src/index.ts
+++ packages/zod-query/src/index.ts
@@ -1,5 +1,14 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 
+// eslint-disable-next-line no-console
+console.log(`
+Thanks for using 'zod-query'! 🎉
+This package is still in development and this version has no features (yet), so please be patient...
+
+If you have any feedback or suggestions, please let me know!
+👉 https://github.com/m10rten/suite/issues
+`);
+
 export namespace ZodQuery {}
 
 export interface QueryClient<T> {
